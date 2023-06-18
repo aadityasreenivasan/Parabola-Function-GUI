7ca4b295d394e39dfdf6609c1f3de326f8b2e798
aadityasreenivasan <104477298+aadityasreenivasan@users.noreply.github.com>
2023-06-18 16:47:56 +0530

    Update Parabola.py

--- Parabola.py
+++ Parabola.py
@@ -9,5 +9,13 @@ def parablola(x):
     return y
 
 
+def draw_axes(canvas):
+    canvas.update()
+    x_origin = canvas.winfo_width() / 2
+    y_origin = canvas.winfo_height() / 2
+    canvas.configure(scrollregion=(-x_origin, -y_origin, x_origin, y_origin))
+    canvas.create_line(-x_origin, 0, x_origin, 0, fill="black")
+    canvas.create_line(0, y_origin, 0, -y_origin, fill="black")
+
 
 
